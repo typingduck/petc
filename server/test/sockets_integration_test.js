0bbd081f5d87e1c1b1b27203ae1c61f2ac1a02b1
Conor Hennessy <typingduck@users.noreply.github.com>
2018-02-11 12:26:47 +0100

    Add socket race condition integration test.

--- server/test/sockets_integration_test.js
+++ server/test/sockets_integration_test.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert')
 const cio = require('socket.io-client')
+const jsonpatch = require('json-patch')
 const request = require('supertest')
 const uuid = require('uuid/v4')
 
@@ -14,7 +15,6 @@ describe('/', () => {
       .expect(200, done)
   })
 })
-
 
 describe('socket clients', () => {
   const serverAddress = request(http).get('/').url
@@ -52,7 +52,6 @@ describe('socket clients', () => {
         client2.emit('doc-patch', client2Patch)
         client2.disconnect()
       })
-
     })
 
     client1.on('doc-patch', msg => {
@@ -67,13 +66,14 @@ describe('socket clients', () => {
   it('should save patches', done => {
     // socket.io messages are async so retry until success a few times
     var retries = 10
-    function testDocument() {
+    function testDocument () {
       request(http)
         .get(`/docs/${docId}`)
         .expect(removeMetaKeys)
         .expect(200, patchedDoc)
         .end((err) => {
           if (retries && err) {
+            retries--
             testDocument()
           } else {
             done(err)
@@ -82,9 +82,7 @@ describe('socket clients', () => {
     }
     testDocument()
   })
-
 })
-
 
 describe('http clients to sockets', () => {
   const serverAddress = request(http).get('/').url
@@ -99,10 +97,6 @@ describe('http clients to sockets', () => {
       .put(`/docs/${docId}`)
       .send(docValue)
       .expect(200, done)
-  })
-
-  after(done => {
-    io.close(done)
   })
 
   it('should broadcast patches to clients', done => {
@@ -127,9 +121,78 @@ describe('http clients to sockets', () => {
   })
 })
 
+describe('socket patch conflicts', () => {
+  const serverAddress = request(http).get('/').url
+  const docId = uuid()
+
+  const docValue = { 'field': 'first' }
+  const client1Patch = { 'op': 'replace', 'path': '/field', 'value': 'patch1' }
+  const client2Patch = { 'op': 'replace', 'path': '/field', 'value': 'patch2' }
+  const client1Doc = docValue
+  const client2Doc = docValue
+
+  before(done => {
+    // Add test doc
+    request(http)
+      .put(`/docs/${docId}`)
+      .send(docValue)
+      .expect(200, done)
+  })
+
+  after(done => {
+    io.close(done)
+  })
+
+  it('clients and db should agree on state', done => {
+    var client1 = cio.connect(serverAddress)
+    client1.on('connect', () => {
+      client1.emit('doc-select', docId)
+      client1.on('doc-patch', patches => {
+        jsonpatch.apply(client1Doc, patches)
+        client1.disconnect()
+      })
+    })
+
+    var client2 = cio.connect(serverAddress)
+    client2.on('connect', () => {
+      client2.emit('doc-select', docId)
+      client2.emit('doc-patch', client2Patch)
+      client1.emit('doc-patch', client1Patch)
+      client2.on('doc-patch', patches => {
+        jsonpatch.apply(client2Doc, patches)
+        client2.disconnect()
+      })
+    })
+
+    function clientsAndDbAgree (dbDoc) {
+      return dbDoc.field !== 'first' &&
+        dbDoc.field === client1Doc.field &&
+        dbDoc.field === client2Doc.field
+    }
+
+    // socket.io messages are async so retry until success a few times
+    var retries = 10
+    function testDocument () {
+      request(http)
+        .get(`/docs/${docId}`)
+        .expect(removeMetaKeys)
+        .expect(200)
+        .end((err, res) => {
+          if (retries && (err || !clientsAndDbAgree(res.body))) {
+            retries--
+            testDocument()
+          } else if (retries) {
+            done()
+          } else {
+            done(Error('db and clients failed to reach consensus'))
+          }
+        })
+    }
+    testDocument()
+  })
+})
 
 function removeMetaKeys (response) {
   delete response.body['_id']
   delete response.body['_rev']
 }
-
