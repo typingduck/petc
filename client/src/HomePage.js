4ba6d9c69398d62839ef93a018dd88f95bb717b9
Conor Hennessy <typingduck@users.noreply.github.com>
2018-02-14 01:59:58 +0100

    Add react render testing.

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
 import './HomePage.css'
+import createNewDoc from './model/model.js'
 
 /**
  * Default home page. Shown before user starts editing document.
  */
-export default function HomePage (props) {
-  return <div></div>
+export default class HomePage extends React.Component {
+  constructor (props) {
+    super(props)
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    console.log('create new')
+    const doc = createNewDoc()
+    console.log('create new: ' + JSON.stringify(doc))
+  }
+
+  render () {
+    return (
+      <div className='petc-homepage'>
+        <a href='#new' onClick={this.handleClick}>Create New</a>
+      </div>
+    )
+  }
 }
--- client/src/HomePage.test.js
+++ client/src/HomePage.test.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HomePage from './HomePage';
+/* global expect it */
+import React from 'react'
+import Renderer from 'react-test-renderer'
+
+import HomePage from './HomePage'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<HomePage />, div);
-});
+  Renderer.create(<HomePage />)
+})
+
+it('matches snapshot', () => {
+  const component = Renderer.create(<HomePage />)
+  expect(component.toJSON()).toMatchSnapshot()
+})
